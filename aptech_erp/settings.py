3eb3674dacada0df00bfb5ec3ec6165f04936df8
Syed Aun Muhammad <aunsyedsyed@gmail.com>
2023-12-13 15:00:49 +0000

    set up checkin_app with intitial template

--- aptech_erp/settings.py
+++ aptech_erp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'checkin_app'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = "aptech_erp.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / 'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
